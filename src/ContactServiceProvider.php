bd2f3accff98412435e8440123b4560e2d75ead0
cipotealuna <93671214+cipotealuna@users.noreply.github.com>
2021-12-13 15:16:23 +0100

    Update ContactServiceProvider.php

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -8,22 +8,29 @@ class ContactServiceProvider extends ServiceProvider
 
     public function boot()
     {
-      $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-      $this->loadViewsFrom(__DIR__.'/views', 'contact');
-      $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-      $this->publishes([__DIR__.'/Http/Controllers' => app_path('Http/Controllers')]);
-      $this->publishes([__DIR__.'/Models' => app_path('Http/Models')]);
-  
+      $this->loadRoutesFrom(__DIR__.'/routes/web.php','contact');
+      $this->loadViewsFrom(__DIR__.'/views','contact');
+      $this->loadMigrationsFrom(__DIR__.'/database/migrations','contact');
 
 
+      //CONTROLADORES
+      $this->publishes([__DIR__.'/Http/Controllers' => app_path('Http/Controllers')],'contact');
 
+      //MODELOS
+      $this->publishes([__DIR__.'/Models' => app_path('Http/Models')],'contact');
+
+      //VISTAS
+      $this->publishes([__DIR__.'/views' => app_path('/resources/views')],'contact');
+
+      //MIGRACIONES
+      $this->publishes([__DIR__.'/database/migrations' => base_path('/database/migrations')],'contact');
+
+     
     }
 
     public function register()
     {
-
-$this->app->make('Ibermedia\Contact\Http\ContactController');
-
+       $this->app->make('Ibermedia\Contact\Http\Controllers\ContactController');
     }
 }
 
